83f43d15b6eededea49ca17ce380398fd11c3030
Anton Minski <anton_minski5@ukr.net>
2021-06-18 13:44:11 +0300

    V.0.10 shop_cart in process

--- store/order/urls.py
+++ store/order/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add_to_cart/<slug:slug>', views.add_to_cart, name='add_to_cart'),
+    path('add_to_cart/<int:id>', views.add_to_cart, name='add_to_cart'),
     path('del_from_cart/<int:id>', views.del_from_cart,
          name='del_from_cart'),
 
